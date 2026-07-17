d4074b1e5f9a671c22e2e3320e9a97d868b6f7d3
Nargiz Soltanova <soltanova-2003@mail.ru>
2026-07-17 12:53:44 +0400

    feat: add env file feat: add DockerFile for services feat: update docker-compose.yml

--- user-service/src/main/java/org/project/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/org/project/userservice/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer ::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**",
+                        .requestMatchers("/api/auth/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**")
